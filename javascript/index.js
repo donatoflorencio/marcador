e60a09f5f6bf756fca019e6fd9dd68530a89313e
donatoflorencio <donato.barros2014@gmail.com>
2022-12-29 23:49:08 -0300

    nova técnica de javascript

--- javascript/index.js
+++ javascript/index.js
@@ -1,17 +1,50 @@
 let menu = document.getElementById("icon-menu");
 let nav = document.getElementById("nav")
 let menuClose = document.getElementById("menu-close")
-let iconNone = document.querySelector(".icon-none")
+let iconsNone = document.querySelectorAll("icons-none")
+let showSidebar = false;
+
+function toggleSidebar() {
+  showSidebar = !showSidebar;
 
 
-  if (nav.style.display = "none") {
+  if(showSidebar) {
+
+    nav.style.display = "block"
+
+    menuClose.style.display = "block"
+
+  } else {
+
+    nav.style.display = "none"
+
+    menuClose.style.display = "none"
+
+  }
+}
+
+function closeSidebar() {
+
+if(showSidebar) {
+
+  toggleSidebar();
+  
+}
+
+}
+
+
+/* if (nav.style.display = "none") {
     menu.addEventListener("click", function() {
       nav.style.display = "block"
-      iconNone.style.display = "none"
+      menuClose.style.display = "block"
+      iconsNone.style.display = "none"
     }
   )
   menuClose.addEventListener("click", function(){
     nav.style.display = "none"
-    iconNone.style.display = "block"
+    
   })
-}
+}*/
+
+
